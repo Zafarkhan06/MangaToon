73faebfb445319ab234900e519efbcfe5b443fdd
Zafar Khan <suparizafar206@gmail.com>
2024-01-04 17:54:12 +0500

    created small cards as well and trending cards as welll

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -2,14 +2,19 @@ import NavigationBar from "../components/NavigationBar/Navigation";
 import FirstSlider from "../components/NavigationBar/Slider/FirstSlider";
 import slidesData from "../components/NavigationBar/Slider/carouselData.json";
 import TrendingCard from "../components/cards/TrendingCard";
+import PopularTab from "../components/PopularTab/PopularTab";
 export default function Home() {
   return (
     <section>
       <div>
-        <NavigationBar></NavigationBar>
-        <div className="px-20 flex flex-row items-center gap-5 justify-start">
+        <NavigationBar />
+        <div className="px-20 flex flex-row items-top gap-5 justify-start">
           <FirstSlider slides={slidesData.slides} />
           <TrendingCard />
+          <div className="px-3 py-4 flex flex-col border border-red-700 justify-start rounded-lg bg-black items-start">
+            <p className="text-white font-semibold text-xl">Popular</p>
+            <PopularTab />
+          </div>
         </div>
       </div>
     </section>
